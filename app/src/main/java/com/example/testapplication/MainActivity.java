d930fb5e48dcdf9d6bd0f9e91b7821cac90bf469
ENSOWT <shivam.gupta@ensowt.com>
2019-11-13 17:06:25 +0530

    new project

---
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.testapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(getApplicationContext(), DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        intent.putExtra(EXTRA_MESSAGE,  editText.getText().toString());
+        startActivity(intent);
+    }
+
+
+}
